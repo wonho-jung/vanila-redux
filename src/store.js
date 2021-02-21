da77ee463751692215dec2f27ba5b535be69f3d3
wonho-jung <wonho.jung92@gmail.com>
2021-02-21 16:43:48 -0500

    redux Tookit vs vailla Redux

--- src/store.js
+++ src/store.js
@@ -1,32 +1,32 @@
 import { createStore } from "redux";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DEL = "DEL";
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DEL");
 
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter((toDO) => toDO.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
 
-const deleteToDo = (id) => {
-  return {
-    type: DEL,
-    id: parseInt(id),
-  };
-};
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DEL:
-      return state.filter((toDO) => toDO.id !== action.id);
-    default:
-      return state;
-  }
-};
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({
+      text: action.payload,
+      id: Date.now(),
+      ...state,
+    });
+  },
+  [deleteToDo]: (state, action) => {
+    state.filter((toDo) => toDo.id !== action.payload);
+  },
+});
 
 const store = createStore(reducer);
 export const actionCreators = {
